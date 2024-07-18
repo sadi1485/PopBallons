5728892f5da08245a54ccd4fcb90c2930862445e
Sadichchha Maharjan <sadimaharjan@Sadichchhas-MacBook-Air.local>
2024-07-18 10:33:00 -0400

    Camera

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const selectCameraIndex=1; // If you think there's more than one camera, pass in the index that you expect the desired one to be.
+const selectCameraIndex=0; // If you think there's more than one camera, pass in the index that you expect the desired one to be.
 var capture;
 var captureReady = false;
 var tracker;
